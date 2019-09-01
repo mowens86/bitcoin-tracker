484254504215f31c026388ca948e3a6a9f20254d
Mike Owens <mocoding86@gmail.com>
2019-09-01 09:42:25 -0700

    complete bit converter

--- index.js
+++ index.js
@@ -15,8 +15,35 @@ app.get("/", (req, res) => {
 
 app.post("/", (req , res) => {
     //console.log(req.body.crypto);
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", (err, res, body) => {
-        console.log(body);
+    const crypto = req.body.crypto;
+    const fiat = req.body.fiat;
+
+
+    const amount = req.body.amount;
+
+    const options = {
+      url: "https://apiv2.bitcoinaverage.com/convert/global",
+      method: "GET",
+      qs: {
+          from: crypto,
+          to: fiat,
+          amount: amount
+      }
+    };
+
+    request(options, (error, respond, body) => {
+        const data = JSON.parse(body);
+        const price = data.price;
+
+        console.log(price);
+
+        const currentDate = data.time;
+
+        res.write(`<p>The current date is ${currentDate}</p>`);
+        res.write(`<h1>${amount} ${crypto} is currently worth ${price} ${fiat}</h1>`);
+
+        res.send();
+        
     });
     
 });
